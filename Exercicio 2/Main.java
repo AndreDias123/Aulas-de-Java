c68892491805bf01c49cca5b0d603ac4d72c46c1
AndreDias123 <andre.dias.146623@a.fecaf.com.br>
2026-09-10 23:32:46 +0000

    Atualiza Main.java do exercicio 2 com o acervo do professor

--- Exercicio 2/Main.java
+++ Exercicio 2/Main.java
@@ -1,32 +1,18 @@
 import java.util.ArrayList;
 
 public class Main {
+
 	public static void main(String[] args) {
 		ArrayList<Livro> livros = new ArrayList<>();
-		livros.add(new Livro("Dom Casmurro", "Machado de Assis", true));
-		livros.add(new Livro("Sao Bernardo", "Graciliano Ramos", true));
-		livros.add(new Livro("1984", "George Orwell", false));
 
-		Biblioteca biblioteca = new Biblioteca("Biblioteca Municipal", livros);
+		livros.add(new Livro("Dom Quixote", "Miguel Servantes", true));
+		livros.add(new Livro("Cem anos de solidao", "Gabriel Garcial", true));
+		livros.add(new Livro("1984", "George Orwel", true));
+		livros.add(new Livro("Memorias Postumas de Bras Cubas", "Machado de Assis", true));
+		livros.add(new Livro("Crime e Castigo", "Fiodor Dostoievski", true));
 
-		Livro novoLivro = new Livro("Clean Code", "Robert C. Martin", true);
-		biblioteca.adicionar_livro(novoLivro);
+		Biblioteca biblioteca = new Biblioteca("Pokemon", livros);
 
-		System.out.println("Acervo atual:");
-		for (Livro livro : livros) {
-			String status = livro.isDisponivel() ? "Disponivel" : "Emprestado";
-			System.out.println("- " + livro.getTitulo() + " (" + livro.getAutor() + ") - " + status);
-		}
-
-		System.out.println();
-		System.out.println("Testando emprestimo e devolucao:");
-		biblioteca.emprestar_livro("Clean Code");
-		biblioteca.emprestar_livro("Clean Code");
-		biblioteca.devolver_livro("Clean Code");
-		biblioteca.devolver_livro("Livro Inexistente");
-
-		System.out.println();
-		System.out.println("Livros disponiveis:");
-		biblioteca.livros_disponiveis();
+		Livro o_livro = new Livro("O pequeno principe", "Antoine", true);
 	}
 }
